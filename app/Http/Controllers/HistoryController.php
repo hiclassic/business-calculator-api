f2e7ba1fcb004206680786f67288f921d6df1074
hiclassic <hares.ahmed.319@gmail.com>
2025-07-25 17:52:08 +0600

    history controler added

---
+++ app/Http/Controllers/HistoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\History;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HistoryController extends Controller
+{
+    public function index()
+    {
+        return History::where('user_id', Auth::id())
+            ->latest()
+            ->take(30)
+            ->get();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'expression' => 'required|string',
+            'result' => 'required|string',
+        ]);
+
+        return History::create([
+            'user_id' => Auth::id(),
+            'expression' => $validated['expression'],
+            'result' => $validated['result'],
+        ]);
+    }
+}
