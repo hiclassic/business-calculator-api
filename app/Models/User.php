495fe74cd327f7b0b21206a1c252557a6e31bc72
hiclassic <hares.ahmed.319@gmail.com>
2025-07-25 18:02:35 +0600

    Sanctum Token Trait

---
+++ app/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, ...; // Add HasApiTokens
+}
